e146b5fe48a6493ca4c5291551de2acfa73c0f6d
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-09-23 15:06:00 -0400

    updated

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -9,11 +9,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          HomeView(),
-        ],
-      ),
+      body: HomeView(),
     );
   }
 }
@@ -30,170 +26,168 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Row(
-        children: [
-          Transform.rotate(
-            angle: pi / 2,
-            child: StepPageIndicator(
-              itemCount: 3,
-              currentPageNotifier: _vPageNotifier,
-              size: 16,
-              onPageSelected: (number) {
-                _vPageController.animateToPage(
-                  number,
-                  curve: Curves.easeIn,
-                  duration: const Duration(milliseconds: 300),
-                );
-              },
-            ),
+    return Row(
+      children: [
+        Transform.rotate(
+          angle: pi / 2,
+          child: StepPageIndicator(
+            itemCount: 3,
+            currentPageNotifier: _vPageNotifier,
+            size: 16,
+            onPageSelected: (number) {
+              _vPageController.animateToPage(
+                number,
+                curve: Curves.easeIn,
+                duration: const Duration(milliseconds: 300),
+              );
+            },
           ),
-          Expanded(
-            child: PageView(
-              scrollDirection: Axis.vertical,
-              controller: _vPageController,
-              onPageChanged: (int index) {
-                _vPageNotifier.value = index;
-                _h1PageNotifier.value = 0;
-              },
-              children: <Widget>[
-                Column(
-                  children: [
-                    // Step Indicator ======================================================
-                    Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: StepPageIndicator(
-                        itemCount: 4,
-                        currentPageNotifier: _h1PageNotifier,
-                        size: 16,
-                        onPageSelected: (page) => {
-                          _h1PageController.animateToPage(
-                            page,
-                            curve: Curves.easeIn,
-                            duration: const Duration(milliseconds: 300),
-                          ),
-                        },
-                      ),
+        ),
+        Expanded(
+          child: PageView(
+            scrollDirection: Axis.vertical,
+            controller: _vPageController,
+            onPageChanged: (int index) {
+              _vPageNotifier.value = index;
+              _h1PageNotifier.value = 0;
+            },
+            children: <Widget>[
+              Column(
+                children: [
+                  // Step Indicator ======================================================
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: StepPageIndicator(
+                      itemCount: 4,
+                      currentPageNotifier: _h1PageNotifier,
+                      size: 16,
+                      onPageSelected: (page) => {
+                        _h1PageController.animateToPage(
+                          page,
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 300),
+                        ),
+                      },
                     ),
-                    // Page View ===========================================================
-                    Expanded(
-                      child: PageView(
-                        scrollDirection: Axis.horizontal,
-                        controller: _h1PageController,
-                        onPageChanged: (int index) {
-                          _h1PageNotifier.value = index;
-                        },
-                        children: <Widget>[
-                          Center(
-                            child: HoverLogo(),
-                          ),
-                          const Center(
-                            child: Text('Second Page'),
-                          ),
-                          const Center(
-                            child: Text('Third Page'),
-                          ),
-                          const Center(
-                            child: Text('Fourth Page'),
-                          )
-                        ],
-                      ),
+                  ),
+                  // Page View ===========================================================
+                  Expanded(
+                    child: PageView(
+                      scrollDirection: Axis.horizontal,
+                      controller: _h1PageController,
+                      onPageChanged: (int index) {
+                        _h1PageNotifier.value = index;
+                      },
+                      children: <Widget>[
+                        Center(
+                          child: HoverLogo(),
+                        ),
+                        const Center(
+                          child: Text('Second Page'),
+                        ),
+                        const Center(
+                          child: Text('Third Page'),
+                        ),
+                        const Center(
+                          child: Text('Fourth Page'),
+                        )
+                      ],
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    // Step Indicator ======================================================
-                    Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: StepPageIndicator(
-                        itemCount: 4,
-                        currentPageNotifier: _h1PageNotifier,
-                        size: 16,
-                        onPageSelected: (page) => {
-                          _h1PageController.animateToPage(
-                            page,
-                            curve: Curves.easeIn,
-                            duration: const Duration(milliseconds: 300),
-                          ),
-                        },
-                      ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  // Step Indicator ======================================================
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: StepPageIndicator(
+                      itemCount: 4,
+                      currentPageNotifier: _h1PageNotifier,
+                      size: 16,
+                      onPageSelected: (page) => {
+                        _h1PageController.animateToPage(
+                          page,
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 300),
+                        ),
+                      },
                     ),
-                    // Page View ===========================================================
-                    Expanded(
-                      child: PageView(
-                        scrollDirection: Axis.horizontal,
-                        controller: _h1PageController,
-                        onPageChanged: (int index) {
-                          _h1PageNotifier.value = index;
-                        },
-                        children: <Widget>[
-                          Center(
-                            child: HoverLogo(),
-                          ),
-                          const Center(
-                            child: Text('Second Page'),
-                          ),
-                          const Center(
-                            child: Text('Third Page'),
-                          ),
-                          const Center(
-                            child: Text('Fourth Page'),
-                          )
-                        ],
-                      ),
+                  ),
+                  // Page View ===========================================================
+                  Expanded(
+                    child: PageView(
+                      scrollDirection: Axis.horizontal,
+                      controller: _h1PageController,
+                      onPageChanged: (int index) {
+                        _h1PageNotifier.value = index;
+                      },
+                      children: <Widget>[
+                        Center(
+                          child: HoverLogo(),
+                        ),
+                        const Center(
+                          child: Text('Second Page'),
+                        ),
+                        const Center(
+                          child: Text('Third Page'),
+                        ),
+                        const Center(
+                          child: Text('Fourth Page'),
+                        )
+                      ],
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    // Step Indicator ======================================================
-                    Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: StepPageIndicator(
-                        itemCount: 4,
-                        currentPageNotifier: _h1PageNotifier,
-                        size: 16,
-                        onPageSelected: (page) => {
-                          _h1PageController.animateToPage(
-                            page,
-                            curve: Curves.easeIn,
-                            duration: const Duration(milliseconds: 300),
-                          ),
-                        },
-                      ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  // Step Indicator ======================================================
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: StepPageIndicator(
+                      itemCount: 4,
+                      currentPageNotifier: _h1PageNotifier,
+                      size: 16,
+                      onPageSelected: (page) => {
+                        _h1PageController.animateToPage(
+                          page,
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 300),
+                        ),
+                      },
                     ),
-                    // Page View ===========================================================
-                    Expanded(
-                      child: PageView(
-                        scrollDirection: Axis.horizontal,
-                        controller: _h1PageController,
-                        onPageChanged: (int index) {
-                          _h1PageNotifier.value = index;
-                        },
-                        children: <Widget>[
-                          Center(
-                            child: HoverLogo(),
-                          ),
-                          const Center(
-                            child: Text('Second Page'),
-                          ),
-                          const Center(
-                            child: Text('Third Page'),
-                          ),
-                          const Center(
-                            child: Text('Fourth Page'),
-                          )
-                        ],
-                      ),
+                  ),
+                  // Page View ===========================================================
+                  Expanded(
+                    child: PageView(
+                      scrollDirection: Axis.horizontal,
+                      controller: _h1PageController,
+                      onPageChanged: (int index) {
+                        _h1PageNotifier.value = index;
+                      },
+                      children: <Widget>[
+                        Center(
+                          child: HoverLogo(),
+                        ),
+                        const Center(
+                          child: Text('Second Page'),
+                        ),
+                        const Center(
+                          child: Text('Third Page'),
+                        ),
+                        const Center(
+                          child: Text('Fourth Page'),
+                        )
+                      ],
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
