190cba0889aadf0ef2d44b93027ebaa46a20c219
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-09-23 15:22:38 -0400

    Update home.dart

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -15,39 +15,62 @@ class Home extends StatelessWidget {
 }
 
 /// This is the stateless widget that the main application instantiates.
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final headers = ['About', 'Our Values', 'Sign-up'];
+  var headerIndex = 0;
+
   final _h1PageController = PageController();
+
   final _vPageController = PageController();
 
   final _h1PageNotifier = ValueNotifier<int>(0);
+
   final _vPageNotifier = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Transform.rotate(
-          angle: pi / 2,
-          child: StepPageIndicator(
-            itemCount: 3,
-            currentPageNotifier: _vPageNotifier,
-            size: 16,
-            onPageSelected: (number) {
-              _vPageController.animateToPage(
-                number,
-                curve: Curves.easeIn,
-                duration: const Duration(milliseconds: 300),
-              );
-            },
-          ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Transform.rotate(
+              angle: pi / 2,
+              child: StepPageIndicator(
+                itemCount: 3,
+                currentPageNotifier: _vPageNotifier,
+                size: 16,
+                onPageSelected: (number) {
+                  _vPageController.animateToPage(
+                    number,
+                    curve: Curves.easeIn,
+                    duration: const Duration(milliseconds: 300),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Text(headers[headerIndex]),
+            ),
+          ],
         ),
         Expanded(
           child: PageView(
             scrollDirection: Axis.vertical,
             controller: _vPageController,
             onPageChanged: (int index) {
+              setState(() {
+                headerIndex = index;
+              });
+
               _vPageNotifier.value = index;
               _h1PageNotifier.value = 0;
             },
